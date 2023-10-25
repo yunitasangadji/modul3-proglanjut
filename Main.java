445feb1a60a4b41e9e208d63a4c17a71c07f22ec
yunitasangadji <sangadjiyunita@gmail.com>
2023-10-25 17:10:44 +0700

    HitungLuasPermukaanBalok

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Membuat objek Scanner untuk menerima input dari pengguna
+        Scanner input = new Scanner(System.in);
+
+        // Meminta pengguna untuk memasukkan panjang, lebar, dan tinggi balok
+        System.out.print("Masukkan panjang balok: ");
+        double panjang = input.nextDouble();
+
+        System.out.print("Masukkan lebar balok: ");
+        double lebar = input.nextDouble();
+
+        System.out.print("Masukkan tinggi balok: ");
+        double tinggi = input.nextDouble();
+        input.close();
+
+        // Menghitung luas permukaan balok
+        double luasPermukaan = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi));
+
+        // Menampilkan hasil
+        System.out.println("Luas permukaan balok adalah: " + luasPermukaan);
     }
 }
