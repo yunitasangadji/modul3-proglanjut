d898da84e4e77909abae57cf3bc6894239d8d378
yunitasangadji <sangadjiyunita@gmail.com>
2023-10-25 16:44:50 +0700

    first commit

---
+++ LuasPermukaanBalok.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+    public class LuasPermukaanBalok {
+        public static void main(String[] args) {
+            // Membuat objek Scanner untuk menerima input dari pengguna
+            Scanner input = new Scanner(System.in);
+
+            // Meminta pengguna untuk memasukkan panjang, lebar, dan tinggi balok
+            System.out.print("Masukkan panjang balok: ");
+            double panjang = input.nextDouble();
+
+            System.out.print("Masukkan lebar balok: ");
+            double lebar = input.nextDouble();
+
+            System.out.print("Masukkan tinggi balok: ");
+            double tinggi = input.nextDouble();
+
+            // Menutup objek Scanner karena sudah selesai digunakan
+            input.close();
+
+            // Menghitung luas permukaan balok
+            double luasPermukaan = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi));
+
+            // Menampilkan hasil
+            System.out.println("Luas permukaan balok adalah: " + luasPermukaan);
+        }
+    }
